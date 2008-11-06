606b0eb4d246989ca5e83b829097a5ddca9acb9c
bili <bili@92701e50-6359-0410-9b06-881c3d199c04>
2008-11-06 12:43:40 +0000

    compilemon adb bili
    git-svn-id: http://svn.alvaromb.com/compilemon/compilemon@8 92701e50-6359-0410-9b06-881c3d199c04

---
+++ src/compilemon.adb
@@ -0,0 +1,46 @@
+--------------------------------------------------------------------------------
+--  *  Prog name compilemon.adb
+--  *  Project name compilemon
+--  *
+--  *  Version 1.0
+--  *  Last update 22/10/08
+--  *
+--  *  Created by Biel Moyà Alcover on 22/10/08.
+--  *  Copyright (c) 2008 __MyCompanyName__.
+--  *  All rights reserved.
+--  *    or (keep only one line or write your own)
+--  *  GNAT modified GNU General Public License
+--  *
+--------------------------------------------------------------------------------
+with Ada.Text_IO;use Ada.Text_IO;
+with decls.d_taula_de_noms; use decls.d_taula_de_noms;
+with decls.dgenerals; use decls.dgenerals;
+procedure compilemon is
+
+	tn : taula_de_noms;
+	id: id_nom;
+begin
+	
+	New_Line(3);
+	tbuida(tn);
+	posa(tn,id,"nili");
+	Put_line("Resultado nili: "&id'img);
+	Put_line("__________________");
+	posa(tn,id,"bili");
+	Put_line("Resultado bili: "&id'img);
+	Put_line("__________________");
+	posa(tn,id,"pepua");
+	Put_line("Resultado pepua : "&id'img);
+	Put_line("__________________");
+	posa(tn,id,"nili");
+	Put_line("Resultado nili : "&id'img);
+	Put_line("__________________");
+	posa(tn,id,"alvaro");
+	Put_line("Resultado alvaro : "&id'img);
+	Put_line("__________________");
+	posa(tn,id,"bili");
+	Put_line("Resultado bili: "&id'img);
+	Put_line("__________________");
+	Put("Taula de caracters:  ");imprimir_tcar(tn,4);
+	
+end compilemon;
