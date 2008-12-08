0b7df1424821303944be67fc6e20c7b0684d8c79
alvaro <alvaro@92701e50-6359-0410-9b06-881c3d199c04>
2008-12-08 20:08:59 +0000

    anyadido decls-tn donde declaramos la taula de noms para que sea visible
    git-svn-id: http://svn.alvaromb.com/compilemon/compilemon@20 92701e50-6359-0410-9b06-881c3d199c04

---
+++ src/decls-tn.ads
@@ -0,0 +1,24 @@
+-- ------------------------------------------------
+--  Paquet de declaracions
+-- ------------------------------------------------
+--  Versió	:	0.1
+--  Autors	:	José Ruiz Bravo
+--				Biel Moyà Alcover
+--				Álvaro Medina Ballester
+-- ------------------------------------------------
+--		En aquest fitxer tenim declarada la
+--  taula de noms perque sigui visible.
+--
+-- ------------------------------------------------
+
+with	decls.d_taula_de_noms;
+
+use		decls.d_taula_de_noms;
+
+
+package decls.tn is
+
+	tn : taula_de_noms;
+
+
+end decls.tn;
