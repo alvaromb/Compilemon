07e0819455499e32da61fe33de7453a32cecfd72
alvaro <alvaro@92701e50-6359-0410-9b06-881c3d199c04>
2009-11-12 19:09:12 +0000

    Principio de documentacion nueva estructurada
    git-svn-id: http://svn.alvaromb.com/compilemon/branches/segona@44 92701e50-6359-0410-9b06-881c3d199c04

--- src/decls.ads
+++ src/decls.ads
@@ -1,18 +1,18 @@
 -- ------------------------------------------------
 --  Paquet de Declaracions
 -- ------------------------------------------------
---  Versió	:	0.1
---  Autors	:	Jose Ruiz Bravo
---				Biel Moya Alcover
---				Alvaro Medina Ballester
+--  Versió      :       0.1
+--  Autors      :       Jose Ruiz Bravo
+--                      Biel Moya Alcover
+--                      Alvaro Medina Ballester
 -- ------------------------------------------------
---		Paquet de declaracions pare.
+--              Paquet de declaracions pare.
 --
 -- ------------------------------------------------
 
 package decls is
 
-	pragma pure;
+   pragma pure;
 
 
 end decls;
