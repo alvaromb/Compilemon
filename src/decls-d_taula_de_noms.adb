5897b3f546a14f68ddec218e19517a5d6ad05c6f
alvaro <alvaro@92701e50-6359-0410-9b06-881c3d199c04>
2009-11-17 16:33:10 +0000

    mes canvis a l'informe i afegit el nou posa_id
    git-svn-id: http://svn.alvaromb.com/compilemon/branches/segona@51 92701e50-6359-0410-9b06-881c3d199c04

--- src/decls-d_taula_de_noms.adb
+++ src/decls-d_taula_de_noms.adb
@@ -1,212 +1,180 @@
 -- ------------------------------------------------
 --  Paquet de declaracions de la taula de noms
 -- ------------------------------------------------
---  Versio	:	0.2
---  Autors	:	Jose Ruiz Bravo
---				Biel Moya Alcover
---				Alvaro Medina Ballester
+--  Versio  :   0.2
+--  Autors  :   Jose Ruiz Bravo
+--              Biel Moya Alcover
+--              Alvaro Medina Ballester
 -- ------------------------------------------------
---		Implementacio dels procediments per al
---	tractament de la taula de noms:
+--  Implementacio dels procediments per al
+-- tractament de la taula de noms:
 --
---			- Buidat de la taula
---			- Insercio
---			- Insercio d'strings
---			- Consulta
+--          - Buidat de la taula
+--          - Insercio
+--          - Insercio d'strings
+--          - Consulta
 --
 -- ------------------------------------------------
 
 
 package body decls.d_taula_de_noms is
 
-	-- Donam els valors per defecte de cada camp, sempre 
-	-- que un camp no sigui utilitzat valdra 0.
-	procedure tbuida (tn : out taula_de_noms) is
-	
-	begin 
-	
-		for i in tn.td'range loop
-			tn.td(i) := id_nul;
-		end loop;
-		
-		tn.nid := 1;
-		tn.ncar := 0;
-		
-		tn.tid(1).seguent := id_nul;
-		
-	end tbuida;
-	
-	
-	
-	function par_iguals (par1, par2 : in string) return boolean is
-	
-		it_p1 : integer;
-		it_p2 : integer;
-		
-	begin
-			
-		if par1'Length = par2'Length then
-			
-			it_p1 := par1'First;
-			it_p2 := par2'First;
-						
-			while it_p1 < par2'Length and par2(it_p2) = par1(it_p1) loop
-				it_p1 := it_p1 + 1;
-				it_p2 := it_p2 + 1;
-			end loop;
-	
-			if par1(it_p1) = par2(it_p2) then
-				return true;
-			end if;
-			
-		end if;
-		
-		return false;
-	
-	end par_iguals;
-		
+    -- Donam els valors per defecte de cada camp, sempre
+    -- que un camp no sigui utilitzat valdra 0.
+    procedure tbuida (tn : out taula_de_noms) is
+
+    begin
+
+        for i in tn.td'range loop
+            tn.td(i) := id_nul;
+        end loop;
+
+        tn.nid := 1;
+        tn.ncar := 0;
+
+        tn.tid(1).seguent := id_nul;
+
+    end tbuida;
 
 
-	procedure posa_id 	(tn : in out taula_de_noms; 
-					   	idn : out id_nom; 
-					   	nom : in string) is
-		
-		-- Variable per el valor de la funcio de dispersio.
-		p_tid : rang_dispersio;
-		
-		-- Índexos per recorrer la taula d'identificadors.
-		idx : id_nom;
-		idx_ant: id_nom; 
-		
-		-- Índex per recorrer la taula de caracters.
-		jdx : rang_tcar; 
-		
-		p : taula_identificadors renames tn.tid;
-		
-	begin
-	
-		p_tid := fdisp_tn(nom);
-		
-		-- Control d'errors
-		if tn.nid = id_nul then
-			raise E_Tids_Plena;
-		end if;
-		
-		if (tn.ncar + nom'Length) > rang_tcar'Last then
-			raise E_Tcar_Plena;
-		end if;
 
-		if tn.td(p_tid) = id_nul then
-			tn.td(p_tid) := tn.nid;
-		end if;
-		
-		idx := tn.td(p_tid);
+    function par_iguals (par1, par2 : in string) return boolean is
 
-		while idx /= id_nul and then not par_iguals(nom, cons_nom(tn, idx)) loop
-			 
-			 if p(idx).seguent = id_nul then
-				idx_ant := idx;
-			 end if;
-			 
-			 idx := p(idx).seguent;	 
-			
-		end loop;
-		
-		if idx = id_nul then
+        it_p1 : integer;
+        it_p2 : integer;
 
-			idn := tn.nid;
-			tn.tid(idx_ant).seguent := idn;
-			
-			-- Apuntam a la primera posicio buida de la taula de caracters
-			tn.tid(idn).pos_tcar := tn.ncar; 
-			tn.tid(idn).long_paraula := nom'Length;
-			tn.tid(idn).seguent := id_nul;
-			tn.nid := tn.nid + 1;
-			-- Omplim la taula de caracters, desde la primera 
-			-- posicio lliure 'ncar'.
-			jdx := tn.ncar;
-			
-			for i in 1..nom'Length loop
-				tn.tc(jdx) := nom(i);
-				jdx := jdx + 1;
-			end loop;
-			
-			jdx := jdx + 1;
-			tn.tc(jdx) := '$';
-			
-			-- Apuntam a la primera posicio lliure de la taula 
-			-- de caracters.
-			tn.ncar := jdx + 1; 
-			
-		else
-			idn := idx;
-			
-		end if;
-		
-	end posa_id;
-	
-	
-	
-	procedure posa_str 	(tn : in out taula_de_noms; 
-							ids : out rang_tcar; 
-							  s : in string) is
-	
-		-- Index per recorrer la taula de caracters.
-		jdx : rang_tcar; 
-		
-	begin
+    begin
 
-		-- Excepcio per a controlar tc plena
-		if (tn.ncar + s'Length) > rang_tcar'Last then
-			raise E_Tcar_Plena;
-		end if;
-	
-		-- Omplim la taula de caracters, desde la primera 
-		-- posicio lliure 'ncar'.
-		jdx := tn.ncar;
-		ids := tn.ncar;
-		
-		for i in 1..s'Length loop
-				
-			tn.tc(jdx) := s(i);
-			jdx := jdx + 1;
-				
-		end loop;
-		
-		tn.ncar := jdx + 1;
-		tn.tc(jdx) := '$';
+        if par1'Length = par2'Length then
 
-	
-	end posa_str;
-	
-	
-	
-	function cons_nom (tn : in taula_de_noms; idn : in id_nom) return string is
-		
-	begin
-	
-		return string(tn.tc(tn.tid(idn).pos_tcar .. tn.tid(idn).pos_tcar+rang_tcar(tn.tid(idn).long_paraula)-1));
-		
-	end cons_nom;	
-	
-	
-	
-	function cons_str	(tn : in taula_de_noms; ids : in rang_tcar) return string is
-	
-		idx : rang_tcar;
-		
-	begin
-		
-		idx := ids;
-	
-		while (tn.tc(idx) /= '$') loop
-			
-			idx := idx+1;
-		end loop;
-		
-		return string(tn.tc(ids..idx-1));
-		
-	end cons_str;
-				
+            it_p1 := par1'First;
+            it_p2 := par2'First;
+
+            while it_p1 < par2'Length and par2(it_p2) = par1(it_p1) loop
+                it_p1 := it_p1 + 1;
+                it_p2 := it_p2 + 1;
+            end loop;
+
+            if par1(it_p1) = par2(it_p2) then
+                return true;
+            end if;
+
+        end if;
+
+        return false;
+
+    end par_iguals;
+
+
+
+    procedure posa_id   (tn : in out taula_de_noms;
+                        idn : out id_nom;
+                        nom : in string) is
+
+        -- Variable per el valor de la funcio de dispersio.
+        p_tid : rang_dispersio;
+
+        -- Indexos per recorrer la taula d'identificadors.
+        idx : id_nom;
+        boolean : trobat;
+
+        -- Index per recorrer la taula de caracters.
+        jdx : rang_tcar;
+
+        p : taula_identificadors renames tn.tid;
+
+    begin
+
+       p_tid := fdisp_tn(nom);
+       Idx := Tn.Td(P_Tid);
+
+       while not Trobat and Idx/=Id_Nul loop
+          if (Nom = Cons_Nom(Tn, Idx)) then
+             Trobat := True;
+          else
+             Idx := Tn.Tid(Idx).Seguent;
+          end if;
+       end loop;
+
+       if not Trobat then
+          Idx := Tn.Nid;
+
+          Tn.Tid(Idx).Pos_Tcar := Tn.Ncar;
+          Tn.Tid(Idx).Seguent := Tn.Td(P_Tid);
+          Tn.Tid(Idx).Long_Paraula := Nom'Length;
+
+          Tn.Td(P_Tid) := Tn.Nid;
+
+          Tn.Nid := Tn.Nid + 1;
+          Tn.Tcar(Tn.Ncar .. Tn.Ncar + Nom'Length-1) := Nom;
+          Tn.Ncar := Tn.Ncar + Nom'Lengt;
+          Tn.Tcar(Tn.Ncar) := $;
+          Tn.Ncar := Tn.Ncar + 1;
+       end if;
+
+    end posa_id;
+
+
+
+    procedure posa_str  (tn : in out taula_de_noms;
+                            ids : out rang_tcar;
+                              s : in string) is
+
+        -- Index per recorrer la taula de caracters.
+        jdx : rang_tcar;
+
+    begin
+
+        -- Excepcio per a controlar tc plena
+        if (tn.ncar + s'Length) > rang_tcar'Last then
+            raise E_Tcar_Plena;
+        end if;
+
+        -- Omplim la taula de caracters, desde la primera
+        -- posicio lliure 'ncar'.
+        jdx := tn.ncar;
+        ids := tn.ncar;
+
+        for i in 1..s'Length loop
+
+            tn.tc(jdx) := s(i);
+            jdx := jdx + 1;
+
+        end loop;
+
+        tn.ncar := jdx + 1;
+        tn.tc(jdx) := '$';
+
+
+    end posa_str;
+
+
+
+    function cons_nom (tn : in taula_de_noms; idn : in id_nom) return string is
+
+    begin
+
+        return string(tn.tc(tn.tid(idn).pos_tcar .. tn.tid(idn).pos_tcar+rang_tcar(tn.tid(idn).long_paraula)-1));
+
+    end cons_nom;
+
+
+
+    function cons_str   (tn : in taula_de_noms; ids : in rang_tcar) return string is
+
+        idx : rang_tcar;
+
+    begin
+
+        idx := ids;
+
+        while (tn.tc(idx) /= '$') loop
+            idx := idx+1;
+        end loop;
+
+        return string(tn.tc(ids..idx-1));
+
+    end cons_str;
+
 
 end decls.d_taula_de_noms;
--- src/decls-d_taula_de_noms.ads
+++ src/decls-d_taula_de_noms.ads
@@ -1,73 +1,76 @@
 -- ------------------------------------------------
 --  Paquet de declaracions de la taula de noms
 -- ------------------------------------------------
---  Versio	:	0.1
---  Autors	:	Jose Ruiz Bravo
---				Biel Moya Alcover
---				Alvaro Medina Ballester
+--  Versio  :   0.1
+--  Autors  :   Jose Ruiz Bravo
+--              Biel Moya Alcover
+--              Alvaro Medina Ballester
 -- ------------------------------------------------
---	Especificacio de l'estructura necessaria
+--  Especificacio de l'estructura necessaria
 -- per el maneig de la taula de noms i dels metodes
 -- per tractar-la.
 --
 -- ------------------------------------------------
 
-with 	decls.dgenerals,
-		decls.d_hash; 
-		
-use 	decls.dgenerals,
-		decls.d_hash;
+with    decls.dgenerals,
+        decls.d_hash;
+
+use     decls.dgenerals,
+        decls.d_hash;
 
 
 package decls.d_taula_de_noms is
 
-	pragma pure;
-	
-	-- Excepcions
-	E_Tids_Plena : exception;
-	E_Tcar_Plena : exception;
-	
-	type taula_de_noms is limited private;
-	
-	procedure tbuida	(tn : out taula_de_noms);
-	
-	procedure posa_id 	(tn : in out taula_de_noms;  
-						idn : out id_nom; 
-						nom : in string);
-	
-	procedure posa_str	(tn : in out taula_de_noms;
-						ids : out rang_tcar;
-						  s : in string);
-						
-	function cons_nom	(tn : in taula_de_noms; 
-						idn : in id_nom) return string;
-	
-	function cons_str	(tn : in taula_de_noms; 
-						ids : in rang_tcar) return string;
-								
-	
-	private
-		
-		type t_identificador is record 
-				pos_tcar : rang_tcar;
-				 seguent : id_nom;
-		  	long_paraula : Natural;
-		end record;
-		
-		type taula_identificadors is array (1 .. id_nom'Last) of t_identificador;
-		
-		type taula_caracters is array (rang_tcar) of character;
-		
-		type taula_de_noms is record
-			  td : taula_dispersio;
-			 tid : taula_identificadors;
-			  tc : taula_caracters;
-			 nid : id_nom;
-			ncar : rang_tcar;
-		end record;
-		
-		-- Funcio de comparacio de dues paraules
-		function par_iguals (par1, par2 : in string) return boolean;		
-					
-			
-end decls.d_taula_de_noms;		
+    pragma pure;
+
+    -- Excepcions
+    E_Tids_Plena : exception;
+    E_Tcar_Plena : exception;
+
+    type taula_de_noms is limited private;
+
+    procedure tbuida    (tn : out taula_de_noms);
+
+    procedure posa_id   (tn : in out taula_de_noms;
+                        idn : out id_nom;
+                        nom : in string);
+
+    procedure posa_str  (tn : in out taula_de_noms;
+                        ids : out rang_tcar;
+                          s : in string);
+
+    function cons_nom   (tn : in taula_de_noms;
+                        idn : in id_nom) return string;
+
+    function cons_str   (tn : in taula_de_noms;
+                        ids : in rang_tcar) return string;
+
+
+    private
+
+        type t_identificador is record
+                pos_tcar : rang_tcar;
+                 seguent : id_nom;
+            long_paraula : Natural;
+        end record;
+
+        type taula_identificadors is array
+          (1 .. id_nom'Last) of t_identificador;
+
+        type taula_caracters is array (rang_tcar)
+          of character;
+
+        type taula_de_noms is record
+            td : taula_dispersio;
+           tid : taula_identificadors;
+            tc : taula_caracters;
+           nid : id_nom;
+          ncar : rang_tcar;
+        end record;
+
+        -- Funcio de comparacio de dues paraules
+        function par_iguals (par1, par2 : in string)
+                            return boolean;
+
+
+end decls.d_taula_de_noms;
